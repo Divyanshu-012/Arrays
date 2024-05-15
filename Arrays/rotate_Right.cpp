411300a41becc5dc8302010512d6e70cee7a169d
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-05-15 18:55:28 +0530

    rotate

---
+++ Arrays/rotate_Right.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> temp(nums.size());
+        int n = nums.size();
+        for(int i = 0; i < nums.size(); i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+        nums = temp;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    
+    cout << "Original array: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    sol.rotate(nums, k);
+    
+    cout << "Array after rotation by " << k << " positions: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
