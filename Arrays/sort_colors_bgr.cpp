77bf881e2b8ff9877b8aad9b9f0de4f8d946c38b
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-05-26 18:31:01 +0530

    bfbg

---
+++ Arrays/sort_colors_bgr.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) cnt0++;
+            else if (nums[i] == 1) cnt1++;
+            else cnt2++;
+        }
+
+        // Replace the places in the original array:
+        for (int i = 0; i < cnt0; i++) nums[i] = 0; // replacing 0's
+        for (int i = cnt0; i < cnt0 + cnt1; i++) nums[i] = 1; // replacing 1's
+        for (int i = cnt0 + cnt1; i < n; i++) nums[i] = 2; // replacing 2's
+    }
+};
+
+int main() {
+    // Example usage
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    
+    Solution sol;
+    sol.sortColors(nums);
+    
+    cout << "Sorted colors: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
