28439cbbc1ad2554eea5dc74b9512f5024ee8b55
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-05-31 04:32:36 +0530

    GVYG

--- Arrays/longestSuccessiveSequence.cpp
+++ Arrays/longestSuccessiveSequence.cpp
@@ -5,6 +5,33 @@
 using namespace std;
 
 
+// **************  OPTIMAL APPROACH  **************
+
+
+// int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
+//     if(n==0) return 0 ;
+//     int lng = 1;
+//     unordered_set<int> st;
+//     for(int  i = 0;i<n;i++){
+//         st.insert(arr[i]);
+//     }
+
+//     for(auto it : st){
+//         if(st.find(it-1) ==st.end()){
+//             int cnt =1;
+//             int x= it;
+//             while(st.find(x+1) != st.end()){
+//                 x = x+1;
+//                 cnt = cnt +1;
+//             }
+//             lng = max(lng,cnt);
+//         }
+//     }
+   
+//     return lng;
+// }
+
+
 // *********  BETTER APPROACH  ***************
 
 int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
