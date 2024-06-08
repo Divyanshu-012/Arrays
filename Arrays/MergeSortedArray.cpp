fda3c8513d76aa3b84a8dc69d65d2d1bd722ec00
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-06-09 04:13:32 +0530

    MergeSortedArray

--- Arrays/MergeSortedArray.cpp
+++ Arrays/MergeSortedArray.cpp
@@ -3,6 +3,48 @@
 
 using namespace std;
 
+
+#include <iostream>
+#include <vector>
+#include <algorithm> // For swap
+
+using namespace std;
+
+//  ************** this solution isn't taking extra space ********
+
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int i = m - 1; // Pointer for the last element in nums1's valid part
+//         int j = n - 1; // Pointer for the last element in nums2
+//         int k = m + n - 1; // Pointer for the last position in nums1
+
+//         // Merge nums2 into nums1 starting from the end
+//         while (i >= 0 && j >= 0) {
+//             if (nums1[i] > nums2[j]) {
+//                 nums1[k] = nums1[i];
+//                 i--;
+//             } else {
+//                 nums1[k] = nums2[j];
+//                 j--;
+//             }
+//             k--;
+//         }
+
+//         // If there are remaining elements in nums2
+//         while (j >= 0) {
+//             nums1[k] = nums2[j];
+//             j--;
+//             k--;
+//         }
+
+//         // No need to handle remaining elements in nums1 as they are already in place
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
